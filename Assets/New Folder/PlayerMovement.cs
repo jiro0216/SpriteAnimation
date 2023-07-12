23e9f21dfd9019f357b00a05cfc0f5aa67b590f6
jiro0216 <athazacreate@gmail.com>
2023-07-12 09:48:32 +0800

    Collect_coin

--- Assets/New Folder/PlayerMovement.cs
+++ Assets/New Folder/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     //Variable  for Animator
     public Animator anim;
 
+    public int coinCounter;
 
 
     // Start is called before the first frame update
@@ -28,61 +29,77 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //While S is pressed run this animation
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            anim.SetTrigger("forwardAnimation");
-            anim.enabled = true;
+        ////While S is pressed run this animation
+        //if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        //{
+        //    anim.SetTrigger("forwardAnimation");
+        //    anim.enabled = true;
 
-        }
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) {
+        //}
+        //if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) {
 
-            anim.enabled = false;
+        //    anim.enabled = false;
 
 
-        }
-        //While W is pressed run this animation
-        if (Input.GetKeyDown(KeyCode.W) ||Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            anim.SetTrigger("backwardAnimation");
-            anim.enabled = true;
-        }
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+        //}
+        ////While W is pressed run this animation
+        //if (Input.GetKeyDown(KeyCode.W) ||Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    anim.SetTrigger("backwardAnimation");
+        //    anim.enabled = true;
+        //}
+        //if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
 
-            anim.enabled = false;
-        }
-        //While A is pressed run this animation
-        if (Input.GetKeyDown(KeyCode.A) ||Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            anim.SetTrigger("leftAnimation");
-            anim.enabled = true;
-        }
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.UpArrow)) {
-            anim.enabled = false;
+        //    anim.enabled = false;
+        //}
+        ////While A is pressed run this animation
+        //if (Input.GetKeyDown(KeyCode.A) ||Input.GetKeyDown(KeyCode.LeftArrow))
+        //{
+        //    anim.SetTrigger("leftAnimation");
+        //    anim.enabled = true;
+        //}
+        //if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.UpArrow)) {
+        //    anim.enabled = false;
 
-        }
-        //While D is pressed run this animation
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            anim.SetTrigger("rightAnimation");
-            anim.enabled = true;
-        }
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            anim.enabled = false;
+        //}
+        ////While D is pressed run this animation
+        //if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        //{
+        //    anim.SetTrigger("rightAnimation");
+        //    anim.enabled = true;
+        //}
+        //if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+        //{
+        //    anim.enabled = false;
+        //}
 
-        }
+        //Gets the horizontal string on animator and adding the movement input x into it.
+        anim.SetFloat("Horizontal", movementInput.x);
+        //Gets the horizontal string on animator and adding the movement input y into it.
+        anim.SetFloat("Vertical",movementInput.y);
+
+        anim.SetFloat("Speed",movementInput.sqrMagnitude);
+
+
 
 
 
     }
-
+    
     //Update that handles physics
     private void FixedUpdate()
     {
         rigidBody.velocity = movementInput * moveSpeed;
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("coin"))
+        {
+            Destroy(collision.gameObject);
+            coinCounter++;
+        }
+    }
     //Converts our Inputs to Vector
     private void OnMove(InputValue inputValue)
     {
